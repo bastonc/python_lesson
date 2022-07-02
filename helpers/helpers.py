6bc78489e26ce4eb5c555e09d590bff476a0feee
Baston Sergey <bastonsv@gmail.com>
2022-07-02 23:13:25 +0300

    HW3 - Adding functionality generating students, save data to file and  output on client by formatted HTML string

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,17 +1,89 @@
+import csv
 import random
 import string
-import pandas
+
+import pandas as pd
+
+from faker import Faker
+
+
+def object_to_str(students_object: list) -> str:
+	"""
+	Formater data from object (list with dictionary) to HTML table
+	:param students_object: list with dict - info about students
+	:return: string - HTML table
+	"""
+	outstring = f'<table border=1, cellspacing=0 cellpadding=5><tr><td align="center">#</td><td align="center">First Name</td>\
+	<td align="center">Last name</td><td align="center">E-mail</td><td align="center">Password</td><td align="center">Birthday</td></tr>'
+	for index, student in enumerate(students_object, start=1):
+		outstring += f'<tr><td>{index}</td><td>{student["first_name"]}</td><td>{student["last_name"]}</td>\
+		<td>{student["email"]}</td><td>{student["password"]}</td><td>{student["birthday"]}</td></tr>'
+	outstring += '</table>'
+	return outstring
 
 
 def gen_pass(long_pass: int) -> str:
-	passwrd = ''.join(random.choice(string.punctuation + string.ascii_letters + string.digits) for _ in range(long_pass))
+	"""
+	Function of password generated
+	:param long_pass: integer dugit - long password
+	:return: string - password
+	"""
+	passwrd = ''.join(
+		random.choice(string.punctuation + string.ascii_letters + string.digits) for _ in range(long_pass))
 	clean_pass = passwrd.replace('<', '&lt;')
 	return clean_pass
 
 
-def read_csv():
-	reader = pandas.read_csv('hw.csv')
+def read_csv() -> dict:
+	"""
+	Read data from hw.csv (hardcode), calculate the average at weight, height
+	:return: dict
+	"""
+	reader = pd.read_csv('hw.csv')
 	avg = reader.mean()
 	return {'avg_weight': round(avg[' Weight(Pounds)'], 2),
 			'avg_height': round(avg[' Height(Inches)'], 2),
 			'len': len(reader)}
+
+
+def store_to_csv(students: list, file_name: str) -> None:
+	"""
+	Write students info into csv file
+	:param students: list with dicts - data for students
+	:param file_name: file name for save to disk
+	:return: None
+	"""
+	with open(file_name + '.csv', 'w', newline='') as csv_f:
+		fieldnames = students[0].keys()
+		writer = csv.DictWriter(csv_f, fieldnames=fieldnames)
+		writer.writeheader()
+		for item_dict in students:
+			writer.writerow(item_dict)
+
+
+def generate_person(amount: int, country_code: str) -> object:
+	"""
+	Generate data about person by Faker
+	:param amount: amount person
+	:param country_code: code by locale
+	:return: list with dicts with data about person
+	"""
+	# first_name, last_name, email, password, birthday
+	fake = Faker(country_code)
+	return [{'first_name': fake.first_name(),
+			 'last_name': fake.last_name(),
+			 'email': fake.email(),
+			 'password': fake.password(),
+			 'birthday': fake.date_between(start_date="-40y", end_date="-18y")} for _ in range(amount)]
+
+
+def get_bitcoin_value():
+	# https://bitpay.com/api/rates
+	# /bitcoin_rate?currency=UAH
+	# input parameter currency code
+	# default is USD
+	# return value currency of bitcoin
+	# * https://bitpay.com/api/
+	# * return symbol of input currency code
+	# * add one more input parameter count and multiply by currency (int)
+	pass
--- main.py
+++ main.py
@@ -1,17 +1,50 @@
-from flask import Flask
-from helpers.helpers import gen_pass, read_csv
 import random
+
+
+from helpers.helpers import gen_pass, read_csv, generate_person, store_to_csv, object_to_str
+
+from http import HTTPStatus
+
+from webargs import fields, validate
+from webargs.flaskparser import use_kwargs
+from flask import Flask, request, jsonify
+
 
 app = Flask(__name__)
 
+@app.errorhandler(HTTPStatus.UNPROCESSABLE_ENTITY)
+@app.errorhandler(HTTPStatus.BAD_REQUEST)
+def error_handler(error):
+	"""
+	Error processing
+	:param error:
+	:return: object
+	"""
+	headers = error.data.get('headers', None)
+	messages = error.data.get('messages', ['incorrect request'])
+	if headers:
+		return jsonify({'errors': messages}, headers, error.code)
+	else:
+		return jsonify({'errors': messages}, error.code)
 
 @app.route('/')
 def index():
-	return '<p> /pass - generated password</p><p>/csv - work from CSV file</p>'
+	"""
+	Index function
+	:return:
+	"""
+	return '<p> /pass - generated password</p><p>/csv - work from CSV file</p>\
+	<p>/students?amount=3&country=EN&file=Student - list students (store to csv)</p>\
+	<p>/btc?currency=USD&change=100 - course and exchange BTC</p>'
 
 
 @app.route('/pass')
 def password():
+	"""
+	Entry point for route /pass
+	(Generating password)
+	:return: string
+	"""
 	len_pass = random.randint(10, 20)
 	passwrd = gen_pass(len_pass)  # use clean function for generated password
 	return f'<p>Password long: {len_pass}</p>Password: {passwrd}'
@@ -19,8 +52,37 @@ def password():
 
 @app.route('/csv')
 def average_csv():
+	"""
+	Entry point for route /csv
+	(Reading from csv and calculating average)
+	:return: string
+	"""
 	avg_data = read_csv()
 	return f"<p>All records in files: {avg_data['len']}</p><p>Average Height(Inches): {avg_data['avg_height']}</p><p>Average Weight(Pounds): {avg_data['avg_weight']}</p>"
+
+
+@app.route('/students')
+@use_kwargs(
+	{
+		'amount': fields.Int(missing=1, validate=[validate.Range(min=1, max=1000)]),
+		'country': fields.Str(missing='UK', validate=[validate.Length(2)]),
+		'file': fields.String(required=True, validate=[validate.Length(max=12), validate.Regexp('^\w+$')])
+	},
+	location='query'
+)
+def generate_students(amount, country, file):
+	"""
+	Entry point for /students with parameters
+	(Adding functionality generating students, save data to file and  output on client by formatted HTML string)
+	:param amount:
+	:param country:
+	:param file:
+	:return:
+	"""
+	students = generate_person(amount=amount, country_code=country)
+	store_to_csv(students, file)
+	outstring = object_to_str(students)
+	return outstring
 
 
 if __name__ == '__main__':
