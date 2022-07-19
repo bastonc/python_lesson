aa0e9268fc22a1c138781fffd68c695d48daad24
Baston Sergey <bastonsv@gmail.com>
2022-07-20 00:14:57 +0300

    HW7 - class SOLID add files

---
+++ main.py
@@ -0,0 +1,9 @@
+from helpers.student import Student
+from helpers.teacher import Teacher
+
+
+if __name__ == '__main__':
+    student1 = Student('Python', 'Sergey', 'Baston', 37, 'py_1', 'Group: py_1 Student')
+    teacher = Teacher('Python', 'Bill', 'Gates', 67, group='py_1', message='Group: py_1 Teacher')
+    student1.readiness_homework(7)
+    teacher.check_homework(student1.get_id(), 7, 100)
